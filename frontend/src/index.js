cf98568ddc0466b9f4047864862093339a7adf82
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-07-10 15:22:30 +0530

    boilerplate of pricing page is added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
 import AboutPage from './landing_page/about/AboutPage';
+import PricingPage from './landing_page/pricing/PricingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <AboutPage />
+      <PricingPage />
     </div>
   </React.StrictMode>
 );
