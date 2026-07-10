78722d41b1e1cfb46734fbe1a3e8abe980087a0c
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-07-10 15:15:58 +0530

    boilerplate of about page is added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
+import AboutPage from './landing_page/about/AboutPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <HomePage />
+      <AboutPage />
     </div>
   </React.StrictMode>
 );
