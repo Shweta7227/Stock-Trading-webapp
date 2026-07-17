15123ea555e67255c30713882de573509220436b
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-07-17 21:58:42 +0530

    Content is added in pricing page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
-import AboutPage from './landing_page/about/AboutPage';
-import PricingPage from './landing_page/pricing/PricingPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <PricingPage />
+      <HomePage/>
     </div>
   </React.StrictMode>
 );
