1b746a780af5cb24dec9a0b7011eef1b846c5f4a
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-07-17 22:06:45 +0530

    heading size is corrected

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -5,7 +5,7 @@ function Pricing() {
         <div className="container">
             <div className="row">
                 <div className="col-4">
-                    <h1 className="mb-3">Unbeatable pricing</h1>
+                    <h1 className="fs-2 mb-3">Unbeatable pricing</h1>
                     <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
                      <a href=""  className="mx-5" style={{textDecoration:"none", color:"#0080ff"}}>
                         See pricing <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
