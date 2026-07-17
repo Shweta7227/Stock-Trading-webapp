502ddc309501a35b3793632188879a2f6ef64177
Shweta7227 <161011928+Shweta7227@users.noreply.github.com>
2026-07-17 18:55:49 +0530

    Award page is built

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React from "react";
+
 function Awards() {
-    return ( 
-        <h1>Awards</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row align-items-center">
+
+        {/* Left Side */}
+        <div className="col-md-6 p-5">
+          <img
+            src="media/images/largestBroker.svg"
+            alt="Award"
+            className="img-fluid"
+          />
+        </div>
+
+        {/* Right Side */}
+        <div className="col-md-6 p-5 mt-3">
+
+          <h1>Largest Stock broker in India</h1>
+
+          <p className="mb-5">
+            2+ million Zerodha clients contribute over 15% of all retail order
+            volume in India daily by trading and investing in:
+          </p>
+
+          <div className="row">
+
+            <div className="col-6">
+              <ul>
+                <li>Futures and Options</li>
+                <li>Commodity derivatives</li>
+                <li>Currency derivatives</li>
+              </ul>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li>Stocks & IPOs</li>
+                <li>Direct mutual funds</li>
+                <li>Bonds and Govt. Securities</li>
+              </ul>
+            </div>
+            
+          </div>
+            <img src="media/images/pressLogos.png" alt="Press Logo" className="img-fluid mt-3" style={{width:"90%"}}/>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
